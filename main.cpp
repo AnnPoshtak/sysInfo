4ffef4ca2fe11836c93b03865978427cf7e32793
AnnPoshtak <annposhtak@gmail.com>
2026-01-02 09:00:00 +0200

    Add proc usage

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
+#include <iomanip>
 
 using namespace std;
 using st = string;
 
+
+struct CPUData {
+	long long total, idle;
+};
+
+CPUData getState() {
+	ifstream file("/proc/stat");
+	st cpuLabel;
+	long long user, nice, system, idle, iowait, irq, softirq, steal;
+
+	if (!(file >> cpuLabel >> user >> nice >> system >>  idle >> iowait >> irq >> softirq >> steal)){
+		return {0, 0};
+	}
+
+	long long total = user + nice +system + idle + iowait + irq + softirq + steal;
+	return {total, idle};
+}
+
 int main(){
+	CPUData prev = getState();
 	ifstream file("/proc/cpuinfo");
 
 	if (!file.is_open()){
@@ -26,5 +47,29 @@ int main(){
 	}
 
 	cout << procName << "\n";
+
+	while (true){
+		usleep(500000);
+
+		CPUData curr = getState();
+		
+		long long diffTotal = curr.total - prev.total;
+		long long diffIdle = curr.idle - prev.idle;
+
+		double percentage = 0.0;
+		if (diffTotal > 0) {
+			percentage = 100.0 * (diffTotal - diffIdle)/diffTotal;
+		}
+
+		cout << "\rLoad CPU usage: " << fixed << setprecision(1) << percentage << "%  " << flush;
+		prev = curr;
+	}
+	return 0;
+
+
+
+
+
+
 	return 0;
 }
